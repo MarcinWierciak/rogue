425e3cd124e2f7ea27e44fa727ada807e67787ab
natahrytsyna <natalya.hrytsyna@gmail.com>
2017-05-18 09:00:53 +0200

    new version

--- game_NM.py
+++ game_NM.py
@@ -1,9 +1,23 @@
 import os
 import sys
 import csv
+import time
 
 
-def print_board(board):
+def print_board(board, end_time):
+    if end_time != 0 :
+        end_timer = '\n',str(int(end_time))
+        board[14] = end_timer
+        #if end_time < 10:
+        #    x = int(end_time)
+        #    y = int(end_time)
+        #else:
+        #    x = int(end_time)/3
+        #    y = int(end_time)/3
+        #    if x % 2 == 0:
+        #        insert_sign(board, x, y, '*')
+        #    else:
+        #        insert_sign(board, x, y, '.')
     for line in board:
         print(''.join(line))
     return board
@@ -45,7 +59,7 @@ def hero_race_def():
 
 
 
-def insert_player(board,x,y,player_sign):
+def insert_sign(board,x,y,player_sign):
     board[y][x] = player_sign
     return board#, y, x
 
@@ -68,7 +82,7 @@ def read_board(filename):
     trees = []
     rowtxt = []
     inv = []
-    text = []
+    timer = []
     with open('sun.txt') as text:
         for line in text:
             line = "\033[1;33m"+ line + "\033[1;m"
@@ -80,6 +94,13 @@ def read_board(filename):
             for char in line[:-1]:
                 if char == '~':
                     char = '\033[94m' + char + '\033[00m'
+                chars = ["M", '+','s', 'h', ':', '/', 'y', 'o', '-', 'd', 'm']
+                if char in chars:
+                    char = '\033[94m' + char + '\033[00m'
+                chars2 = ['N', 'n', 'u', '0']
+                if char in chars2:
+                    char = "\033[1;33m"+ char + "\033[1;m"
+
                 lista.append(char)
             board.append(lista)
     with open('map2.txt') as text:
@@ -90,10 +111,10 @@ def read_board(filename):
     name = "\033[1;36mThe Legend Of Zelda: Cult of Deadly Python\033[1;m:\033[1;31m ♥\033[1;m"
     for i in name:
         rowtxt.append(i)
-    print(rowtxt.index("♥"))
     board.append(rowtxt)
-    print(board.index(rowtxt))
     board.append(inv)
+    board.append(timer)
+
     #board.append(text)
     return board
 
@@ -113,82 +134,85 @@ def add_lifes(board):
     lifes = 0
     lifes += 1
     board[13][63] += lifes*" ♥ "
-    print_board(board)
-
-
-# def moving2(...):
-#     direction = getch()
-#     newx = x
-#     newy = y
-#     if direction == 'd':
-#         newx+=1
-#     elif di...
+    print_board(board, end_time)
+    return lifes
 
 
 
-
-def moving(board, x, y, player_sign, obstacle, border):
+def moving2(board, x, y, player_sign, obstacle, border):
     direction = getch()
+    new_x = x
+    new_y = y
     if direction == "d":
-        current_yx = board[y][x+1]
-        if not current_yx in border:
-            if current_yx == obstacle:
-                insert_player(board, x, y, obstacle)
-            elif current_yx == "❀":
-                add_lifes(board)
-                add_to_inventory(board,'❀')
-            else:
-                insert_player(board, x, y, ".")
-            x += 1
-            #insert_player(board, x, y, player_sign)
+        new_x += 1
     if direction == "a":
-        current_yx = board[y][x-1]
-        if not current_yx in border:
-            if current_yx == obstacle:
-                insert_player(board, x, y, obstacle)
-            else:
-                insert_player(board, x, y, ".")
-            x -= 1
-            #insert_player(board ,x ,y, player_sign)
+        new_x -= 1
     if direction == "w":
-        current_yx = board[y-1][x]
-        if not current_yx in border:
-            if current_yx == obstacle:
-                insert_player(board, x, y, obstacle)
-            else:
-                insert_player(board, x, y, ".")
-            y -= 1
-            #insert_player(board ,x ,y , player_sign)
+        new_y -= 1
     if direction == "s":
-        current_yx = board[y+1][x]
-        if not current_yx in border:
-            if current_yx == obstacle:
-                insert_player(board, x, y, obstacle)
-            else:
-                insert_player(board, x, y, ".")
-            y += 1
-            #insert_player(board, x ,y , player_sign)
+        new_y += 1
+    new_yx = board[new_y][new_x]
+    if not new_yx in border:
+        if new_yx == obstacle:
+            insert_sign(board, x, y, obstacle)
+        elif new_yx == "❀":
+            add_lifes(board)
+            add_to_inventory(board,'❀')
+            insert_sign(board, x, y, ".")
+        else:
+            insert_sign(board, x, y, ".")
+        x = new_x
+        y = new_y
     if direction == "x":
         sys.exit()
     return x, y
 
 
+
+
 def main():
-    #lifes = 3
     border = ['|','_','\033[94m' + '~' + '\033[00m']
     read_file('int_screen.txt')
+    #start_time = time.time()
+
     player_race = hero_race_def()
     player_sign = hero_class_def(player_race)
+
+    #print(round(int(end_time)))
     x= 10
     y= 10
     board = read_board('maps.txt')
-    #board = insert_player(board,x, y, player_sign)
-    #insert_player(board,5, 5, "*")
-    #print_board(board)
+    #dupa = False
+    start_time = 0
+    #lifes = add_lifes(board)
     while True:
-        x, y = moving(board ,x ,y, player_sign, "#", border)
-        board = insert_player(board, x, y, player_sign)
-        os.system('clear')
-        print_board(board)
+        x, y = moving2(board ,x ,y, player_sign, "#", border)
+        if board[y][x] == "^":
+            board = read_board('python.txt')
+            x = 1
+            y = 2
+        elif board[y][x] == ">":
+            board = read_board('test1.txt')
+            x = 1
+            y = 2
+            #dupa = True
+            start_time = time.time()
+            #timer(board, start_time)
+        if start_time != 0:
+            end_time = time.time() - start_time
+        else:
+            end_time = 0
+        #if int(end_time) > 30 and dupa == True:
+        #    sys.exit()
+            #start_time = time.time()
 
-main()
+        insert_sign(board, x, y, player_sign)
+        os.system('clear')
+        print_board(board, end_time)
+        #timer(5)
+
+
+
+
+if __name__ == '__main__':
+    main()
